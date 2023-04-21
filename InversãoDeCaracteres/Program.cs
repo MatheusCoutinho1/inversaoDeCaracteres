a89813f06baf7dd5ecc03a5c8291f7f2ec6089c9
Matheus <matheuscnc4@gmail.com>
2023-04-21 02:39:43 -0300

    Quinta questão do teste

---
+++ InversãoDeCaracteres/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string texto = "Exemplo de texto para inverter";
+
+        char[] caracteres = texto.ToCharArray();
+        int tamanho = caracteres.Length;
+
+        for (int i = 0; i < tamanho / 2; i++)
+        {
+            char temp = caracteres[i];
+            caracteres[i] = caracteres[tamanho - i - 1];
+            caracteres[tamanho - i - 1] = temp;
+        }
+
+        texto = new string(caracteres);
+
+        Console.WriteLine(texto);
+    }
+}
